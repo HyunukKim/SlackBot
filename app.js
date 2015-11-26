fecee2dc7fe2f134956464f651771835afe0695e
HyunukKim <gizingun@gmail.com>
2015-11-26 23:10:07 +0900

    * slackbot 개발 중

--- app.js
+++ app.js
@@ -1,8 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
-var slackBot = require('./core/hellobot');
-//var dicebot = require('./core/dicebot');
-
+var slackBot = require('./core/slackbot');
 
 var app = express();
 var port = process.env.PORT || 3000;
@@ -10,23 +8,20 @@ var port = process.env.PORT || 3000;
 // body parser middleware
 app.use(bodyParser.urlencoded({ extended: true }));
 
+app.listen(port, function () {
+  console.log('Slack bot listening on port ' + port);
+});
+
 // test route
 app.get('/', function (req, res){
 	res.status(200).send('Hello world!');
 });
 
-app.post('/hello', slackBot);
-
-// dicebot
-//app.post('/roll', dicebot);
+app.post('/slackbot', slackBot);
 
 // error handler
 app.use(function (err, req, res, next) {
   console.error(err.stack);
   res.status(400).send(err.message);
-});
-
-app.listen(port, function () {
-  console.log('Slack bot listening on port ' + port);
 });
 
---
+++ core/slackbot.js
@@ -0,0 +1,45 @@
+var diceBot = require('./dicebot');
+
+module.exports = function (req, res, next) {
+  var trigger = req.body.trigger_word;
+  var regx = new RegExp(trigger+'(.*)(\\s)(.*)');
+  var matches = regx.exec(req.body.text);
+  console.log('matches : ' + matches);
+  if (matches) {
+    var command = matches[1]; // 명령어
+    if (command === 'dice') {
+      req.body.text = matches[3];
+      console.log('body text : ' + req.body.text);
+      diceBot(req, res, next);
+      res.status(200).end();
+    }
+    console.log('command : ' + command);
+  } else {
+    var botPayload = {
+      text : '```' + '* * * * * Command List * * * * *\n' + '$dice <number>d<sides> : ex)$dice 2d6 -> 6면 주사위를 2번 굴린다.\n' + '* * * 대소문자 구분 주의 * * * \n' + '```'
+    };
+    res.status(200).json(botPayload);
+  }
+
+
+  // var command = req.body.text.substring(triggerWord.length);
+  // console.log('trigger : ' + triggerWord+'length : ' + triggerWord.length);
+  // console.log('command : ' + command);
+  // console.log('comman1 : ' + command.trim());
+  // if (command.length == 0 || command === '') {
+  //   // 빈 문자열 - trigger word만 존재
+  //   // 명령어 리스트 소개
+
+  // } else {  // 명령 부분
+  //   var matches = command.match(/^(\d{1,2})d(\d{1,2})$/);
+  //   if (matches && matches[1] && matches[2]) {
+  //     req.body.text = command;
+  //     //diceBot(req, res, next);
+  //     return;
+  //   }
+
+  //   console.log('next?');
+  // }
+  // // avoid infinite loop
+  // res.status(200).end();
+}
